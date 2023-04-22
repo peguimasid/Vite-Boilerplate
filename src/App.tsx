2a248a931c43385e8230ac3d3de79617af2c7342
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-22 19:34:30 -0300

    finish boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center bg-slate-900 text-white">
+      <h1>Hello World</h1>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
