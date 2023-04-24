842a94888d95db22883df414502639c71c2a496b
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-24 18:44:35 -0300

    adding material ui

--- src/pages/Public/index.tsx
+++ src/pages/Public/index.tsx
@@ -1,3 +1,4 @@
+import { Button, Icon } from '@mui/material';
 import { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -5,9 +6,9 @@ export const Public: FunctionComponent = () => {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center space-y-3 bg-slate-900 text-white">
       <h3>Public</h3>
-      <Link to="/protected" className="rounded-md bg-blue-400 p-3">
+      <Button component={Link} variant="contained" to="/protected" endIcon={<Icon>chevron_right</Icon>}>
         Go to private page
-      </Link>
+      </Button>
     </main>
   );
 };
