14acbbc2f478dd2bd3bf490a9aeb4d1f39615bb2
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-24 18:53:49 -0300

    add custom theme

--- src/pages/Public/index.tsx
+++ src/pages/Public/index.tsx
@@ -6,7 +6,13 @@ export const Public: FunctionComponent = () => {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center space-y-3 bg-slate-900 text-white">
       <h3>Public</h3>
-      <Button component={Link} variant="contained" to="/protected" endIcon={<Icon>chevron_right</Icon>}>
+      <Button
+        component={Link}
+        variant="contained"
+        color="secondary"
+        to="/protected"
+        endIcon={<Icon>chevron_right</Icon>}
+      >
         Go to private page
       </Button>
     </main>
